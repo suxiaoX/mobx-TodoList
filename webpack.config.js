8dbd681e052a46491280e5f9d97680d6ec8a1987
leosu <leosu@shunshunliuxue.com>
2017-04-11 14:32:16 +0800

    项目结构

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const NODE_MODULES_PATH = path.resolve(__dirname, 'node_modules');
+
+module.exports = {
+  devtool:'eval',
+  entry:[
+    'webpack-hot-middleware/client?reload=true',
+    './src/client'
+  ],
+  output:{
+    path:path.join(__dirname,'dist'),
+    filename:'bundle.js',
+    puclicPath:'/static/'
+  },
+  plugins:[
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV) },
+      __CLIENT__: JSON.stringify(true),
+      __SERVER__: JSON.stringify(false),
+    }),
+  ],
+  resolve:{
+    extensions: ['.js', '.jsx']
+  },
+  module:{
+    loaders:[
+      {
+        test:/\.jsx?$/,
+        loaders:['babel-loader'],
+        exclude:NODE_MODULES_PATH
+      }
+    ]
+  }
+}
